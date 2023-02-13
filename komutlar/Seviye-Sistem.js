5f6835f393d8e08c5a6eba9fb28a83d8b4334f85
Glitch (shadowrise) <none>
2023-02-13 19:40:04 +0000

    🏨💆 Checkpoint ./komutlar/Seviye-Sistem.js:54625839/844

--- komutlar/Seviye-Sistem.js
+++ komutlar/Seviye-Sistem.js
@@ -4,58 +4,58 @@ let prefix = ayarlar.prefix;
 
 exports.run = async (bot, msg, args) => {
   const seviye = new Discord.MessageEmbed()
-    .setAuthor(`𝐁𝐢𝐠$𝐁𝐨𝐬𝐬 | Seviye Sistem`)
+    .setAuthor(`ReiXdc/PREMİUM | Seviye Sistem`)
     .setTitle(``)
     .setColor("#00ff00")
     .setThumbnail(
-      "https://cdn.discordapp.com/attachments/1011348727371403316/1018273039030440026/standard_8.gif"
-    )
-    .setDescription(
-      `<:aworld:985798359929466920> 𝐁𝐢𝐠$𝐁𝐨𝐬𝐬 Botumuzu Eklemek İçin \`${prefix}davet\` yazabilirsiniz.`
+      "https://cdn.discordapp.com/attachments/1050853850740965508/1074728770050719824/Reix_Ozel_Bot_Photo.png"
     )
     .addField(
+      ` ReiXdc/PREMİUM Botumuzu Eklemek İçin \`${prefix}davet\` yazabilirsiniz.`
+    .setDescription(
+    )
       `**__Seviye__**`,
-      `<:aworld:985798359929466920> \`${prefix}seviye\` \n Sizin Özelliştirdiginiz Seviye Sıralama Kartını Gösterir.`,
+      ` \`${prefix}seviye\` \n Sizin Özelliştirdiginiz Seviye Sıralama Kartını Gösterir.`,
         true
     )
      .addField(
       `**__Seviye Sıralama__**`,
-      `<:aworld:985798359929466920> \`${prefix}sıralama\` \n Seviye DataBase 'indeki Sıralama Ranklarını Görürsün.`,
+      ` \`${prefix}sıralama\` \n Seviye DataBase 'indeki Sıralama Ranklarını Görürsün.`,
         true
     )
     .addField(
       `**__SeviyeKart Özelleştir Resim__**`,
-      `<:aworld:985798359929466920> \`${prefix}seviyekart-özelleştir resim\` \n Seviye Kartınıza Özel Arka Plan Ayarlarsınız.`,
+      ` \`${prefix}seviyekart-özelleştir resim\` \n Seviye Kartınıza Özel Arka Plan Ayarlarsınız.`,
         true
     )
    .addField(
       `**__SeviyeKart Özelleştir Renk__**`,
-      `<:aworld:985798359929466920> \`${prefix}seviyekart-özelleştir renk\` \n Seviye Kartınıza Özel Renk Ayarlarsınız.`,
+      ` \`${prefix}seviyekart-özelleştir renk\` \n Seviye Kartınıza Özel Renk Ayarlarsınız.`,
         true
     )
     .addField(
       `**__Seviye Aç__**`,
-      `<:aworld:985798359929466920> \`${prefix}seviye-ayarla durum aç\` \n Discord Sunucunuzda Seviye Sistemini Açarsınız.`,
+      ` \`${prefix}seviye-ayarla durum aç\` \n Discord Sunucunuzda Seviye Sistemini Açarsınız.`,
         true
     )
      .addField(
       `**__Seviye Kapat__**`,
-      `<:aworld:985798359929466920> \`${prefix}seviye-ayarla durum kapat\` \n Discord Sunucunuzda Seviye Sistemini Kapatırsınız.`,
+      ` \`${prefix}seviye-ayarla durum kapat\` \n Discord Sunucunuzda Seviye Sistemini Kapatırsınız.`,
         true
     )
      .addField(
       `**__Seviye Kanal__**`,
-      `<:aworld:985798359929466920> \`${prefix}seviye-ayarla log kanal\` \n Discord Sunucunuzda Seviye Log Kanal Ayarlarsanız.`,
+      ` \`${prefix}seviye-ayarla log kanal\` \n Discord Sunucunuzda Seviye Log Kanal Ayarlarsanız.`,
         true
     )
      .addField(
       `**__Seviye Mesaj__**`,
-      `<:aworld:985798359929466920> \`${prefix}seviye-ayarla log mesaj seviye\` \n Seviye Log Kanalına Gidecek Mesaji Ayarlasınız.`,
+      ` \`${prefix}seviye-ayarla log mesaj seviye\` \n Seviye Log Kanalına Gidecek Mesaji Ayarlasınız.`,
         true
     )
     .addField(
       `__Bilgilendirme__`,
-      `<:aworld:985798359929466920>  \`${prefix}davet\` | Botu Sununuya Davet Edersiniz\n <a:yesil:822529538663514173> \`${prefix}botbilgi\` | Botun İstatistiklerini Görürsünüz \n <a:yesil:822529538663514173> \`${prefix}iletişim\` | 𝐁𝐢𝐠$𝐁𝐨𝐬𝐬 İletişim Bilgileri.`
+      `  \`${prefix}davet\` | Botu Sununuya Davet Edersiniz\n  \`${prefix}botbilgi\` | Botun İstatistiklerini Görürsünüz \n  \`${prefix}iletişim\` | ReiXdc/PREMİUM İletişim Bilgileri.`
     );
   msg.channel.send(seviye);
 };
