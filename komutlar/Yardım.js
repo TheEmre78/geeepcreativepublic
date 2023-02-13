db454fac74a8faef39cf9f5749759ed14b9bb03e
Glitch (shadowrise) <none>
2023-02-13 16:53:03 +0000

    ⛩🚓 Checkpoint ./gweepcreative.js:54625839/2 ./ayarlar.json:54625839/181 ./komutlar/Yardım.js:54625839/636

--- komutlar/Yardım.js
+++ komutlar/Yardım.js
@@ -5,48 +5,48 @@ exports.run = async (client, message) => {
   let prefix = ayarlar.prefix;
 
   const embed = new Discord.MessageEmbed()
-    .setAuthor(`𝐁𝐢𝐠$𝐁𝐨𝐬𝐬 DESTEK`)
+    .setAuthor(`ReiXdc/PREMİUM DESTEK`)
     .setTitle(``)
     .setColor("#00ff00")
     .setThumbnail(
-      "https://cdn.discordapp.com/attachments/1011348727371403316/1018273039030440026/standard_8.gif"
+      "https://cdn.discordapp.com/attachments/1050853850740965508/1074728770050719824/Reix_Ozel_Bot_Photo.png"
     )
     .setDescription(
-      `<a:aaworld:979833178934108170> 𝐁𝐢𝐠$𝐁𝐨𝐬𝐬 Botumuzu Eklemek İçin \`${prefix}davet\`<a:mavitik:1022210829304746076> yazabilirsiniz.`
+      ` ReiXdc/PREMİUM Botumuzu Eklemek İçin \`${prefix}davet\` yazabilirsiniz.`
     )
     .addField(
       `__Genel Komutlar__`,
-      `<a:earthblack:1022207943116066887> \`${prefix}genel\``,
+      ` \`${prefix}genel\``,
       true
     )
     .addField(
       `__Ayarlar Komutlar__`,
-      `<a:ayar:1022209437026484244>  \`${prefix}ayarlar\` `,
+      `  \`${prefix}ayarlar\` `,
       true
     )
     .addField(
       `__Kullanıcı Komutlar__`,
-      `<a:leise_oksag:1022208279222431814> \`${prefix}kullanıcı\` `,
+      ` \`${prefix}kullanıcı\` `,
       true
     )
     .addField(
       `__Çekiliş Komutlar__`,
-      `<a:aacekilis:1022208053514350654> \`${prefix}çekiliş\` `,
+      ` \`${prefix}çekiliş\` `,
       true
     )
     .addField(
       `__Eklenti Komutlar__`,
-      `<a:AyarGif:1022208780072656977> \`${prefix}eklenti\``,
+      ` \`${prefix}eklenti\``,
       true
     )
     .addField(
       `__Eğlence Komutlar__`,
-      `<a:goyun:1022208380170932365> \`${prefix}eğlence\``,
+      ` \`${prefix}eğlence\``,
       true
     )
     .addField(
       `__Bilgilendirme__`,
-      `<a:mavitik:1022210829304746076>  \`${prefix}davet\` | Botu Sununuya Davet Edersiniz\n <a:emoji_12:979725849731407873> \`${prefix}botbilgi\` | Botun İstatistiklerini Görürsünüz \n <a:emoji_12:979725813740093440> \`${prefix}iletişim\` | 𝐁𝐢𝐠$𝐁𝐨𝐬𝐬  İletişim Bilgileri.`
+      `  \`${prefix}davet\` | Botu Sununuya Davet Edersiniz\n  \`${prefix}botbilgi\` | Botun İstatistiklerini Görürsünüz \n  \`${prefix}iletişim\` | ReiXdc/PREMİUM  İletişim Bilgileri.`
     );
   return message.channel.send(embed);
 };
